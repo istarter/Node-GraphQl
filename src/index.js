e0bb1601897eadbc76c66c94d8c04a4f2153df30
Najeeb <naxibkhan@gmail.com>
2021-07-11 13:58:15 +0500

    GraphQl mutation implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
+import { uuid } from "uuidv4";
 
 // import express
 const app = express();
@@ -7,7 +8,7 @@ const app = express();
 // writing queries and mutions here.
 const schema = gql`
   type Query {
-    name: String
+    whoami: String
     class: String
     students: [Student]
     findStudent(sid: ID!): Student
@@ -16,33 +17,53 @@ const schema = gql`
     name: String
     id: ID
   }
+  type Mutation {
+    addStudent(name: String!): Student
+    deleteStudent(sid: ID!): ID
+  }
 `;
 
 // Global array object
-const studenList = [
-  { name: "Mohsin", id: 1 },
-  { name: "Fahad", id: 2 },
-  { name: "Kabir", id: 3 },
-  { name: "Safi", id: 4 },
-];
+const studentList = [{ name: "Mohsin", id: 1 }];
+
 // Query that get argument and return something query name findStudent.
 
 const resolvers = {
   Query: {
-    name: () => {
+    whoami: (_, args, { currentStd }) => {
+      console.log(currentStd);
       return "Najib";
     },
     class: () => {
       return "BSCS";
     },
     students: () => {
-      return studenList;
+      return studentList;
     },
     findStudent: (_, args) => {
-      const result = studenList.find((stdObj) => {
+      const result = studentList.find((stdObj) => {
         return stdObj.id == args.sid;
       });
       return result;
+    },
+  },
+  Mutation: {
+    addStudent: (_, args) => {
+      const lastId = studentList[studentList.length - 1].id;
+      const newStudent = {
+        name: args.name,
+        // id: lastId + 1,
+        id: uuid(),
+      };
+      studentList.push(newStudent);
+      return newStudent;
+    },
+    // deleting user
+    deleteStudent: (_, args) => {
+      const stdObj = studentList.find((sObj) => sObj.id == args.sid);
+      studentList.splice(stdObj.id - 1, 1);
+      console.log(studentList);
+      return stdObj.id;
     },
   },
 };
@@ -51,6 +72,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    currentStd: studentList[2],
+  },
 });
 
 // url for the graphlql, end point
